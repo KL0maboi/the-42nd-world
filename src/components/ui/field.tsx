1ec5c06f1f58ea4eb1c8bae796e294df9b79593d
KL0maboi <shakshatho@gmail.com>
2026-09-08 13:46:14 +0530

    fix some minor tailwind warnings

--- src/components/ui/field.tsx
+++ src/components/ui/field.tsx
@@ -105,7 +105,7 @@ function FieldLabel({
     <Label
       data-slot="field-label"
       className={cn(
-        'group/field-label peer/field-label flex gap-2 has-[>[data-slot=field]]:not-has-[:disabled,[data-disabled]]:hover:bg-muted/50 has-data-checked:bg-primary/5 dark:has-data-checked:bg-primary/10 group-data-[disabled=true]/field:opacity-50 *:data-[slot=field]:p-2.5 has-[>[data-slot=field]]:border has-[>[data-slot=field]]:has-focus-visible:border-ring has-data-checked:border-primary/30 dark:has-data-checked:border-primary/20 has-[>[data-slot=field]]:rounded-lg has-[>[data-slot=field]]:has-[:focus-visible]:ring-3 has-[>[data-slot=field]]:has-[:focus-visible]:ring-ring/50 w-fit leading-snug',
+        'group/field-label peer/field-label flex gap-2 has-[>[data-slot=field]]:not-has-[:disabled,[data-disabled]]:hover:bg-muted/50 has-data-checked:bg-primary/5 dark:has-data-checked:bg-primary/10 group-data-[disabled=true]/field:opacity-50 *:data-[slot=field]:p-2.5 has-[>[data-slot=field]]:border has-[>[data-slot=field]]:has-focus-visible:border-ring has-data-checked:border-primary/30 dark:has-data-checked:border-primary/20 has-[>[data-slot=field]]:rounded-lg has-[>[data-slot=field]]:has-focus-visible:ring-3 has-[>[data-slot=field]]:has-focus-visible:ring-ring/50 w-fit leading-snug',
         'has-[>[data-slot=field]]:w-full has-[>[data-slot=field]]:flex-col',
         className,
       )}
